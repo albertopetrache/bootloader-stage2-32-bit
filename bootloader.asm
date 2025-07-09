f8c638dbf5dcf26d8de4227a33e81c57d9a53a1f
unknown <alberto.petrache99@gmail.com>
2025-07-09 21:42:38 +0200

    Update

--- bootloader.asm
+++ bootloader.asm
@@ -14,31 +14,17 @@ reset_disk:
     int 0x13            ; BIOS interrupt
     jc reset_disk       ; Retry if carry flag is set (error)
 
-; Display main message
-mov si, msg_hello
-call print_string
-
-; Display "Loading" with progress bar
-mov si, msg_loading
-call print_string
-
-; Initialize progress bar
-call init_progress_bar
-
-; Simulate progress (instead of real disk read)
-mov cx, 20                 ; 20 steps for the progress bar
-progress_loop:
-    call update_progress_bar
-    push cx
-    mov cx, 0x000F         ; Short delay between steps
-    call delay
-    pop cx
-    loop progress_loop
+mov ax, 1
+mov ax, 2
+mov ax, 3
+mov ax, 4
+mov ax, 5
+mov ax, 7 
 
 ; Read stage2
 read_stage2:
     mov ah, 0x02           ; BIOS read sectors function
-    mov al, 1              ; Number of sectors to read
+    mov al, 4              ; Number of sectors to read
     mov ch, 0              ; Cylinder 0
     mov dh, 0              ; Head 0
     mov cl, 2              ; Sector 2 (immediately after bootloader)
@@ -47,7 +33,7 @@ read_stage2:
     jc read_stage2         ; Retry on error
 
 ; Jump to stage2
-jmp 0x0000:0x7E00
+jmp 0x0000:0x8000
 
 ; Text printing routine
 print_string:
@@ -61,64 +47,6 @@ print_string:
     jmp .loop
 .done:
     ret
-
-; Delay routine
-; Input: CX = outer loop counter (duration)
-delay:
-    push dx
-    mov dx, 0x0032         ; Inner loop counter
-.delay_loop:
-    dec dx
-    jnz .delay_loop
-    dec cx
-    jnz .delay_loop
-    pop dx
-    ret
-
-; Progress bar routine  
-init_progress_bar:
-    pusha
-    mov ah, 0x03           ; Get cursor position
-    mov bh, 0x00           ; Page 0
-    int 0x10
-    
-    ; Save cursor position for progress bar
-    mov [progress_bar_line], dh
-    mov [progress_bar_col], dl
-    
-    ; Print empty progress bar
-    mov si, progress_bar_empty
-    call print_string
-    popa
-    ret
-
-update_progress_bar:
-    pusha
-    ; Set cursor position
-    mov ah, 0x02
-    mov bh, 0x00
-    mov dh, [progress_bar_line]
-    mov dl, [progress_bar_col]
-    int 0x10
-    
-    ; Print one segment of the progress bar
-    mov si, progress_bar_segment
-    call print_string
-    
-    ; Update column position
-    inc byte [progress_bar_col]
-    popa
-    ret
-
-; Messages
-msg_hello db 'Booting MyOS...', 13, 10, 0
-msg_loading db 'Loading: ', 0
-progress_bar_empty db '[                    ]', 0 ; 20 spaces
-progress_bar_segment db 0xDB, 0 ; Block character for progress
-
-; Variables for progress bar
-progress_bar_line db 0
-progress_bar_col db 0
-
+	
 times 510 - ($-$$) db 0
 dw 0xAA55                 ; Boot signature
--- stage2.asm
+++ stage2.asm
@@ -1,5 +1,5 @@
-[org 0x8000]
 [bits 16]
+[org 0x8000]
 
 start:
     cli                     ; Disable interrupts
@@ -26,51 +26,118 @@ init_pm:
 
     ; Display initial message
     call clear_screen
+    
+    ; Display welcome message
     mov esi, msg
     mov edi, 0xB8000        ; VGA text mode buffer
     mov ah, 0x0F            ; Text attribute: white on black
     call print_string_pm
-	mov edi, 0xB8000 + 160  ; Move to second line (80 chars * 2 bytes)
-
-	; Display msg1
-	mov esi, msg1	
-	mov ah, 0x0F
-	call print_string_pm
-	
-	mov esi, buffer
-	mov eax, [x]            ; Load number into EAX
+    
+    ; Display number message
+    mov edi, 0xB8000 + 160  ; Move to second line (80 chars * 2 bytes)
+    mov esi, msg1
+    mov ah, 0x0F
+    call print_string_pm
+    
+    ; Convert number to string
+    mov esi, buffer
+    mov eax, [x]            ; Load number into EAX
 convert:
-	xor edx, edx
-	mov ebx, 10
-	div ebx                 ; Divide EAX by 10, remainder in EDX
-	add dl, '0'             ; Convert remainder to ASCII
-	mov [esi], dl
-	inc esi
-	inc dword [lungime_buffer]
-	test eax, eax           ; If EAX == 0, done
-	jnz convert
-	
-	; Reverse the buffer (number is currently in reverse order)
-	mov esi, 0
-	mov edi, [lungime_buffer]
-	dec edi
+    xor edx, edx
+    mov ebx, 10
+    div ebx                 ; Divide EAX by 10, remainder in EDX
+    add dl, '0'             ; Convert remainder to ASCII
+    mov [esi], dl
+    inc esi
+    inc dword [lungime_buffer]
+    test eax, eax           ; If EAX == 0, done
+    jnz convert
+    
+    ; Reverse the buffer (number is currently in reverse order)
+    mov esi, 0
+    mov edi, [lungime_buffer]
+    dec edi
 invert:
-	mov al, [buffer + esi]
-	mov bl, [buffer + edi]
-	mov [buffer + esi], bl
-	mov [buffer + edi], al
-	inc esi
-	dec edi
-	cmp esi, edi
-	jnge invert
-	
-	; Display buffer
-	mov edi, 0xB8000 + 160
-	add edi, msg1_len * 2    ; Position after msg1
-	mov esi, buffer
-	mov ah, 0x0F
-	call print_string_pm
-
+    mov al, [buffer + esi]
+    mov bl, [buffer + edi]
+    mov [buffer + esi], bl
+    mov [buffer + edi], al
+    inc esi
+    dec edi
+    cmp esi, edi
+    jl invert
+    
+    ; Display buffer
+    mov edi, 0xB8000 + 160
+    add edi, msg1_len * 2    ; Position after msg1
+    mov esi, buffer
+    mov ah, 0x0F
+    call print_string_pm
+    
+    ; Display progress bar label
+    mov edi, 0xB8000 + 320  ; Third line
+    mov esi, progress_msg
+    mov ah, 0x0F
+    call print_string_pm
+    
+    ; Initialize progress bar
+    mov edi, 0xB8000 + 320 + progress_msg_len * 2  ; After "Loading... "
+    mov ecx, 20             ; Progress bar width
+    mov ah, 0x07            ; Gray on black for empty part
+    mov al, ' '             ; Space character
+.init_bar:
+    stosw                   ; Store empty progress bar
+    loop .init_bar
+    
+    ; Draw progress bar with delay
+    mov esi, 0              ; Current progress
+.progress_loop:
+    cmp esi, 20             ; Total progress steps
+    jge .progress_done
+    
+    ; Draw filled part
+    mov edi, 0xB8000 + 320 + progress_msg_len * 2  ; Start of progress bar
+    mov ecx, esi            ; Number of filled segments
+    inc ecx
+    mov ah, 0x4F            ; White on red for filled part
+    mov al, 0xDB            ; Block character
+.draw_filled:
+    mov [edi + (ecx-1)*2], ax ; Write to video memory
+    loop .draw_filled
+    
+    ; Calculate percentage
+    mov eax, esi
+    mov ebx, 5              ; Multiply by 5 (since 100%/20 = 5% per step)
+    mul ebx
+    mov ebx, eax
+    
+    ; Display percentage
+    push esi
+    mov edi, 0xB8000 + 320 + progress_msg_len * 2 + 42  ; Position for percentage
+    mov esi, percent_buffer
+    call int_to_string
+    
+    ; Display % sign
+    mov ax, 0x0F25          ; '%' character with white-on-black
+    mov [edi + 4], ax
+    pop esi
+    
+    ; Short delay to see the update
+    push esi
+    mov ecx, 0x0020FFFFF     ; Delay count
+    call delay
+    pop esi
+    
+    inc esi
+    jmp .progress_loop
+    
+.progress_done:
+    ; Display completion message
+    mov edi, 0xB8000 + 480  ; Fourth line
+    mov esi, complete_msg
+    mov ah, 0x0F
+    call print_string_pm
+    
     ; Halt the CPU
     cli
     hlt
@@ -87,19 +154,69 @@ clear_screen:
 print_string_pm:
     lodsb                   ; Load byte from [ESI] to AL
     test al, al
-    jz .done
+    jz .done    
     stosw                   ; Store AL and AH to [EDI]
     jmp print_string_pm
 .done:
     ret
 
+; Convert integer in EBX to string at ESI
+int_to_string:
+    pusha
+    mov eax, ebx
+    mov ecx, 0
+    mov edi, esi
+    
+    ; Handle zero case
+    test eax, eax
+    jnz .convert_loop
+    mov byte [edi], '0'
+    inc edi
+    jmp .null_terminator
+    
+.convert_loop:
+    xor edx, edx
+    mov ebx, 10
+    div ebx                 ; Divide EAX by 10, remainder in EDX
+    add dl, '0'             ; Convert remainder to ASCII
+    push dx
+    inc ecx
+    test eax, eax           ; If EAX == 0, done
+    jnz .convert_loop
+    
+.store_loop:
+    pop ax
+    mov [edi], al
+    inc edi
+    loop .store_loop
+    
+.null_terminator:
+    mov byte [edi], 0       ; Null terminator
+    popa
+    ret
+
+; Delay routine
+; Input: ECX = delay count
+delay:
+    push ecx
+.delay_loop:
+    dec ecx
+    jnz .delay_loop
+    pop ecx
+    ret
+    
 ; Data
 msg db 'Hello, World from 32-bit protected mode!', 0
 msg1 db 'The number is: '
 msg1_len equ $ - msg1
 x dd 938765
 lungime_buffer dd 0
-buffer db 0, 0, 0, 0        ; Space to store number string (max 10 digits)
+buffer times 10 db 0
+percent_buffer times 10 db 0  ; Buffer for percentage string
+
+progress_msg db 'Loading... [                    ] ', 0
+progress_msg_len equ $ - progress_msg - 23  ; Length without the bar spaces
+complete_msg db 'Loading complete!', 0
 
 ; GDT (Global Descriptor Table)
 gdt_start:
